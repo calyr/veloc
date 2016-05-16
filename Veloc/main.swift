5f500d212a228c7519badd70a436d37850a98f46
calyr <calyr_software@hotmail.com>
2016-05-15 22:21:41 -0400

    oki

--- Veloc/main.swift
+++ Veloc/main.swift
@@ -1,6 +1,6 @@
 //
 //  main.swift
-//  Veloc
+//  CuartaSemana
 //
 //  Created by calyr on 5/15/16.
 //  Copyright © 2016 calyr. All rights reserved.
@@ -8,5 +8,53 @@
 
 import Foundation
 
-print("Hello, World!")
+enum Velocidades : Int  {
+    case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
+    init(velocidadInicial:Velocidades){
+        self = velocidadInicial
+    }
+}
+
+
+class Auto{
+    var velocidades : Velocidades
+    
+    init(velocidad: Velocidades){
+        self.velocidades = velocidad
+    }
+    
+    func cambioDeVelocidad() -> (velocidad:Int, texto:String){
+        var texto = "";
+        var temporal = Velocidades(velocidadInicial: Velocidades.Apagado )
+        switch velocidades{
+        case .Apagado :
+            texto = "Apagado"
+            temporal = self.velocidades
+            self.velocidades = .VelocidadBaja;
+            
+        case .VelocidadBaja:
+            texto = "Velocidad Baja"
+            temporal = self.velocidades
+            self.velocidades = .VelocidadMedia
+            
+        case .VelocidadMedia:
+            texto = "Velocidad Media"
+            temporal = self.velocidades
+            self.velocidades = .VelocidadAlta
+            
+        case .VelocidadAlta:
+            texto = "Velocidad Alta"
+            temporal = self.velocidades
+            self.velocidades = .VelocidadMedia
+            
+        }
+        return (temporal.rawValue, texto)
+    }
+}
+
+
+var miauto = Auto(velocidad: .Apagado)
+for( var i = 0; i < 20; i++){
+    print( miauto.cambioDeVelocidad() )
+}
 
